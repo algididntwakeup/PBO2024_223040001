df2b5f4784d1f3e2063fdccdcd92b486982f69bc
algididntwakeup <algisosmedacc@gmail.com>
2024-03-28 22:30:14 +0700

    upload tugas pertemuan 7

---
+++ PBO7/Mahasiswa.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PBO7;
+
+/**
+ *
+ * @author algi
+ */
+public class Mahasiswa {
+    private String NRP;
+    private String nama;
+    
+    public Mahasiswa(String nrp, String nama){
+        this.NRP = nrp;
+        this.nama = nama;
+    }
+    
+    public String display(){
+     return "NRP: " + NRP + " Nama : " + nama; 
+    }
+    
+    
+//    SETTER DAN GETTER
+
+    public String getNRP() {
+        return NRP;
+    }
+
+    public void setNRP(String NRP) {
+        this.NRP = NRP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    
+}
